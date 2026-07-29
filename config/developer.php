ffb6a69419a53da0a12c7391c7236c2d70c65df1
Fazal <fazalabbas5172@gmail.com>
2026-07-29 17:14:10 +0500

    defined contact developer info

---
+++ config/developer.php
@@ -0,0 +1,36 @@
+<?php
+
+define(
+    'DEV_NAME',
+    'Fazal Abbas Shah'
+);
+
+
+define(
+    'DEV_WHATSAPP',
+    '923058061272'
+);
+
+
+define(
+    'DEV_EMAIL',
+    'fazalabbas5172@gmail.com'
+);
+
+
+define(
+    'DEV_LINKEDIN',
+    'https://www.linkedin.com/in/fazal111/'
+);
+
+
+define(
+    'DEV_GITHUB',
+    'https://github.com/Fazal5172'
+);
+
+
+define(
+    'DEV_RESUME',
+    'downloads/Fazal-Abbas-Shah-Resume.pdf'
+);
